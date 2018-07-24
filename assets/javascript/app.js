c41df93eeaadc07f2f109898bbe4087319e5ae1a
Joseph Venuto <jbvenuto@gmail.com>
2018-07-24 18:03:15 -0400

    Put questions and answers into html file

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+//Global variables
+
+
+//Start quiz when the user clicks the start button
+$("#start").click()
+
+//Timer that begins when the page loads
+
+
+//Checks if the user selected the correct answer
+
+//Add a point to the correct answer score
+
+//Add a point to the incorrect answer score
