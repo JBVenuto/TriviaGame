f5b17247fff527f4c373ab2fd92578f08cfc4730
Joseph Venuto <jbvenuto@gmail.com>
2018-07-25 16:59:33 -0400

    included countdown timer at the top of the quiz

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,5 @@
 //Global variables
+var time = 30;
 var correct = 0;
 var wrong = 0;
 var noAnswer = 0;
@@ -6,10 +7,12 @@ var btName = ["bt1", "bt2", "bt3", "bt4", "bt5", "bt6", "bt7", "bt8", "bt9", "bt
 var answers = [];
 
 $(document).ready(function() {
-    //Start quiz when the user clicks the start button
-    $("#start").click()
 
     //Timer that begins when the page loads
+    setInterval(function(){
+        time--;
+        $("#timer").html("<h2>Time Remaining: " + time + "</h2>")}, 1000
+    );
 
     //Action to take when the submit button is pressed
     $("#submit").click(function() {
@@ -23,26 +26,25 @@ $(document).ready(function() {
             if(bVal == 1){
                 correct++;
             }
+            //Add a point to the wrong answer score
             else if(bVal == 0){
                 wrong++;
             }
+            //Add a point to the not answered score
             else {
                 noAnswer++;
             }
-        }
+        };
             //prints the answers array to console
             console.log(answers);
             console.log("Number of correct answers: " + correct);
             console.log("Number of wrong answers: " + wrong);
             console.log("Number of questions not answered: " + noAnswer);
         
-        //Add a point to the correct answer score
-
-        //Add a point to the incorrect answer score
-
         //Show the number of right and wrong answers
+        $("#quiz").html("<h3>Correct Answers: " + correct + "</h3><h3>Incorrect Answers: " + wrong + "</h3><h3>Unasnwerd Questions: " + noAnswer + "</h3>");
 
-    })
+    });
 });
 
 //store button names in an array
